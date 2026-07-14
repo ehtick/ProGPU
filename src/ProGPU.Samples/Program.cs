59a9e34f9a119f4545fc5ef6cea0003f1f1a2cde
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-07-14 23:10:59 +0200

    Right-size sample glyph browser cache

--- src/ProGPU.Samples/Program.cs
+++ src/ProGPU.Samples/Program.cs
@@ -23,7 +23,7 @@ public class App : Application
         window.Title = "ProGPU Substrate - High-Performance WinUI Gallery Dashboard";
         window.Width = 1280;
         window.Height = 800;
-        window.GlyphAtlasSize = 3072;
+        window.GlyphAtlasSize = 2560;
 
         window.Activated += (s, e) =>
         {
