64a182e3d6f447ba97d69de395787dcf591de066
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-07-24 11:13:20 +0200

    Add standalone Avalonia integration samples

---
+++ samples/RenderDemo/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq.Expressions;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Rendering;
+using RenderDemo.ViewModels;
+using MiniMvvm;
+
+namespace RenderDemo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var vm = new MainWindowViewModel();
+
+            void BindOverlay(Expression<Func<MainWindowViewModel, bool>> expr, RendererDebugOverlays overlay)
+                => vm.WhenAnyValue(expr).Subscribe(x =>
+                {
+                    var diagnostics = RendererDiagnostics;
+                    diagnostics.DebugOverlays = x ?
+                        diagnostics.DebugOverlays | overlay :
+                        diagnostics.DebugOverlays & ~overlay;
+                });
+
+            BindOverlay(x => x.DrawDirtyRects, RendererDebugOverlays.DirtyRects);
+            BindOverlay(x => x.DrawFps, RendererDebugOverlays.Fps);
+            BindOverlay(x => x.DrawLayoutTimeGraph, RendererDebugOverlays.LayoutTimeGraph);
+            BindOverlay(x => x.DrawRenderTimeGraph, RendererDebugOverlays.RenderTimeGraph);
+
+            DataContext = vm;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
